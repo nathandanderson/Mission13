48cc17eea24d0cf3073449af78f6873ab69cbd2c
Nathan Anderson <nathand.anderson@outlook.com>
2022-03-30 11:21:03 -0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Mission13.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission13.Controllers
+{
+    public class HomeController : Controller
+    { 
+        private BowlersDbContext _context { get; set; }
+        public HomeController(BowlersDbContext temp)
+        {
+            _context = temp;
+        }
+
+
+        public IActionResult Index()
+        {
+            ViewBag.Teams = _context.Teams.OrderBy(x => x.TeamName).ToList();
+
+            var blah = _context.Bowlers
+                .Include(x => x.Team)
+                .ToList();
+
+            return View(blah);
+        }
+
+         
+        public IActionResult TeamView(int TeamID)
+        {
+            ViewBag.Teams = _context.Teams.OrderBy(x => x.TeamName).ToList();
+
+            int drugs = Convert.ToInt32(RouteData?.Values["teamid"]);
+            var drugz = _context.Teams.Single(x => x.TeamID == drugs);
+            ViewBag.Name = drugz;
+
+            var blah = _context.Bowlers
+                .Include(x => x.Team)
+                .Where(x => x.TeamID == TeamID)
+                .ToList();
+            return View("Index", blah);
+        }
+
+
+        [HttpGet]
+        public IActionResult NewBowler()
+        {
+            ViewBag.Teams = _context.Teams.OrderBy(x => x.TeamName).ToList();
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult NewBowler(Bowler b)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Update(b);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.Teams = _context.Teams.OrderBy(x => x.TeamName).ToList();
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Edit (int BowlerID)
+        {
+            ViewBag.Teams = _context.Teams.OrderBy(x => x.TeamName).ToList();
+            var bowl = _context.Bowlers.Single(x => x.BowlerID == BowlerID);
+
+            return View("NewBowler", bowl);
+        }
+
+        [HttpPost]
+        public IActionResult Edit (Bowler b)
+        {
+            _context.Update(b);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+
+
+
+        //Render the delete page for a given bowler
+        [HttpGet]
+        public IActionResult Delete(int BowlerID)
+        {
+            var task = _context.Bowlers.Single(x => x.BowlerID == BowlerID);
+
+            return View(task);
+        }
+
+        //Actually delete the bowler after asking for confirmation
+        [HttpPost]
+        public IActionResult Delete(Bowler b)
+        {
+            _context.Bowlers.Remove(b);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/Bowler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission13.Models
+{
+    public class Bowler
+    {
+        [Key]
+        [Required]
+        public int BowlerID { get; set; }
+        [MaxLength(50)]
+        public string BowlerFirstName { get; set; }
+        [MaxLength(50)]
+        public string BowlerLastName { get; set; }
+        [MaxLength(1)]
+        public string BowlerMiddleInit { get; set; }
+        [MaxLength(50)]
+        public string BowlerAddress { get; set; }
+        [MaxLength(50)]
+        public string BowlerCity { get; set; }
+        [MaxLength(2)]
+        public string BowlerState { get; set; }
+
+        [MaxLength(10)]
+        public string BowlerZip { get; set; }
+        [MaxLength(14)]
+        public string BowlerPhoneNumber { get; set; }
+        public int TeamID { get; set; }
+        public Team Team { get; set; }
+
+    }
+}
